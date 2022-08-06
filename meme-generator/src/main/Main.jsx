503b66d5b65cdc9b6da5f3fe746839e786af4189
Fakevin0613 <ke_qinyang@outlook.com>
2022-08-05 23:28:43 -0400

    basic setup

--- meme-generator/src/main/Main.jsx
+++ meme-generator/src/main/Main.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Grid, TextField, Button,Typography } from '@mui/material'
+import mainStyle from './Style'
 
 const Main = () => {
+  const classes = mainStyle();
+
   return (
-    <div>Main</div>
+    <div className={classes.entirePage}>
+      <div className={classes.grid}>
+        <Grid container spacing={2} className={classes.grid}>
+          <Grid item xs={12}>
+            <Typography variant="h4" color="initial" className={classes.title}>Try to make your own meme!</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              className={classes.top}
+              helperText="Text Limit"
+              label="Top Text"
+            />
+          </Grid>
+
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              className={classes.bottom}
+              helperText="Text Limit"
+              label="Bottom Text"
+
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button fullWidth variant="outlined" className={classes.clear}>Clear</Button>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button fullWidth variant="contained" className={classes.generate}>Generate</Button>
+          </Grid>
+
+        </Grid>
+      </div>
+    </div>
+
   )
 }
 
---
+++ meme-generator/src/main/Style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@mui/styles";
+
+const mainStyle = makeStyles({
+    entirePage:{
+        margin: "100px 20vw 0 20vw",
+        userSelect: "none",
+    },
+    grid:{
+        justifyContent: "center",
+    },
+    title: {
+        textAlign: "center",
+    },
+})
+
+export default mainStyle
